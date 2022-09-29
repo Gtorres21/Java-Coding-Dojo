b29112460438158ba6a02f1e74a0ca28b7b9f039
Gtorres21 <giovannytorres21@gmail.com>
2022-09-28 19:40:32 -0700

    Completed Assignment Zoo Keeper: Part 1

---
+++ spring/ZooKeeperOne/src/com/giovanni/zookeeperone/ZooKepperOneApplication.java
@@ -0,0 +1,31 @@
+package com.giovanni.zookeeperone;
+import com.giovanni.zookeeperone.models.Mammal;
+import com.giovanni.zookeeperone.models.Gorilla;
+
+public class ZooKepperOneApplication {
+
+	public static void main(String[] args) {
+		
+//		Mammal animal1 = new Mammal(100);
+//		animal1.displayEnergy();
+//		
+//		Mammal animal2 = new Mammal();
+//		animal2.displayEnergy();
+		
+		Gorilla monkey1 = new Gorilla();
+		monkey1.setEnergy(100);
+		monkey1.throwSomething();
+		monkey1.throwSomething();
+		monkey1.throwSomething();
+		monkey1.eatBananas();
+		monkey1.eatBananas();
+		monkey1.climb();
+//		100 - (15) + (20) - 10 = 95;
+		monkey1.getEnergy();
+	
+
+		
+		
+	}
+
+}
---
+++ spring/ZooKeeperOne/src/com/giovanni/zookeeperone/models/Mammal.java
@@ -0,0 +1,40 @@
+package com.giovanni.zookeeperone.models;
+
+public class Mammal {
+	
+//	attributes
+	protected int energy;
+
+	
+//	Constructor
+	public Mammal() {}
+	
+	public Mammal(int energy) {
+		this.energy = energy;	
+	}
+	
+//	Methods
+	public int displayEnergy() {
+//		System.out.printf("Energy Level: %d", this.energy);
+		return this.energy;
+	}
+	
+
+	
+
+
+//	Getters and Setters
+	public int getEnergy() {
+		System.out.printf("\n Energy Level: %d", this.energy);
+		return this.energy;
+	}
+
+	public void setEnergy(int energy) {
+		this.energy = energy;
+	}	
+	
+	
+	
+	
+
+}
